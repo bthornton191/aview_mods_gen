ca21a0917741546c37324c76bf3d76ef2d680848
Ben Thornton <ben.thornton@hexagon.com>
2023-12-12 09:54:17 -0500

    generalized file paths for icons in Windows context menus

--- add_windows_context_menus.bat
+++ add_windows_context_menus.bat
@@ -1,5 +1,5 @@
 REG ADD HKEY_CLASSES_ROOT\directory\background\shell\adams_cmd /VE /T REG_SZ /D "Open Adams cmd" /F
-REG ADD HKEY_CLASSES_ROOT\directory\background\shell\adams_cmd /V "Icon" /T REG_SZ /D "C:\Users\ben.thornton\Pictures\icons\cmd.ico" /F
+REG ADD HKEY_CLASSES_ROOT\directory\background\shell\adams_cmd /V "Icon" /T REG_SZ /D "%%USERPROFILE%%\Pictures\icons\cmd.ico" /F
 REG ADD HKEY_CLASSES_ROOT\directory\background\shell\adams_cmd\command /VE /T REG_EXPAND_SZ /D ""%%ADAMS_LAUNCH_COMMAND%%\..\..\..\aview_mods_gen\open_adams_cmd_in_folder.bat" "%%1"" /F
 
 REG ADD HKEY_CLASSES_ROOT\directory\background\shell\aview /VE /T REG_SZ /D "Open Adams View" /F
@@ -7,7 +7,7 @@ REG ADD HKEY_CLASSES_ROOT\directory\background\shell\aview /V "Icon" /T REG_EXPA
 REG ADD HKEY_CLASSES_ROOT\directory\background\shell\aview\command /VE /T REG_EXPAND_SZ /D ""%%ADAMS_LAUNCH_COMMAND%%\..\..\..\aview_mods_gen\open_adams_view_in_folder.bat" "%%v"" /F
 
 REG ADD HKEY_CLASSES_ROOT\directory\shell\adams_cmd /VE /T REG_SZ /D "Open Adams cmd" /F
-REG ADD HKEY_CLASSES_ROOT\directory\shell\adams_cmd /V "Icon" /T REG_SZ /D "C:\Users\ben.thornton\Pictures\icons\cmd.ico" /F
+REG ADD HKEY_CLASSES_ROOT\directory\shell\adams_cmd /V "Icon" /T REG_SZ /D "%%USERPROFILE%%\Pictures\icons\cmd.ico" /F
 REG ADD HKEY_CLASSES_ROOT\directory\shell\adams_cmd\command /VE /T REG_EXPAND_SZ /D ""%%ADAMS_LAUNCH_COMMAND%%\..\..\..\aview_mods_gen\open_adams_cmd_in_folder.bat" "%%1"" /F
 
 REG ADD HKEY_CLASSES_ROOT\directory\shell\aview /VE /T REG_SZ /D "Open Adams View" /F
