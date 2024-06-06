6db79e18d7e157c19673fe8f662938e90ebb7055
Ben Thornton <ben.thornton@hexagon.com>
2024-06-06 09:11:39 -0400

    added a free body diagram macro and a show all force graphics macro

--- aviewBS.cmd
+++ aviewBS.cmd
@@ -22,6 +22,8 @@ macro read macro_name = .athorn.mid_marker file="C:\Program Files\MSC.Software\A
 macro read macro_name = .athorn.hide_others file="C:\Program Files\MSC.Software\Adams\aview_mods_gen\hide_others.mac" user="athorn hide_others"
 macro read macro_name = .athorn.ppt_winmax file="C:\Program Files\MSC.Software\Adams\aview_mods_gen\ppt_winmax.mac" user="athorn ppt_winmax"
 macro read macro_name = .athorn.pick_from_screen file="C:\Program Files\MSC.Software\Adams\aview_mods_gen\pick_from_screen.mac" user="athorn pick_from_screen"
+macro read macro_name = .athorn.fbd file="C:\Program Files\MSC.Software\Adams\aview_mods_gen\fbd.mac" user="athorn fbd" create_panel=yes
+macro read macro_name = .athorn.all_force_graphics file="C:\Program Files\MSC.Software\Adams\aview_mods_gen\all_force_graphics.mac" user="athorn all_force_graphics" create_panel=no
 
 ! Set Environment Variables
 var set var=.mdi.tmp_int int=(eval(putenv("MDI_ONLINE_BROWSER", "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")))
@@ -39,6 +41,7 @@ int btn_stack undisplay btn_stack_name = .gui.main.standard_toolbar.dyn_view_tra
 int btn_stack undisplay btn_stack_name = .gui.main.standard_toolbar.dyn_tra_depth 
 int push_button undisplay push_button_name = .gui.main.standard_toolbar.view_zoom 
 int field display field_name = .gui.main.standard_toolbar.*
+
 ! ---------------------------------------------------------------------
 ! Run Startup Scripts
 ! ---------------------------------------------------------------------
@@ -71,6 +74,55 @@ default plt_attributes table_font_size=5
 default plotting axis_numbers_font_size=5
 default plotting axis_label_font_size=5
 default report base_font_size=5
+
+! ---------------------------------------------------------------------
+! Add Plotting Tools
+! ---------------------------------------------------------------------
+for var=.mdi.menu_bar  obj=(eval(db_immediate_children(.gui.ppt_main, "graphic_interface_menu_bar")))
+
+    ! Hotkeys
+    interface separator create separator_name=(eval(.mdi.menu_bar // ".tools.hotkeys_sep"))
+    interface push_button create &
+        push_button_name=(eval(.mdi.menu_bar // ".tools.btn_plot_tracking")) &
+        label = "Plot Tracking::Ctrl+T" &
+        commands = "interface push_button execute push_button_name = .gui.ppt_main.ppt_tbar1.plotting_cont.tracking action = single_click"
+
+    interface push_button create   &
+        push_button_name=(eval(.mdi.menu_bar // ".tools.btn_treeview_visibility")) &
+        label = "Toggle Sidebar::Shift+Ctrl+E" &
+        commands = "if condition=(.gui.ppt_main.sash1.treeview.displayed)", &
+                "    mdi toolbar display toolbar=treeview state=off", &
+                "    int tog set state=off tog=.gui.ppt_main.ppt_tbar1.treeview_visibility", &
+                    "else", &
+                    "    mdi toolbar display toolbar=treeview state=on", &
+                    "    int tog set state=on tog=.gui.ppt_main.ppt_tbar1.treeview_visibility", &
+                    "end" !"
+
+    interface push_button create &
+        push_button_name=(eval(.mdi.menu_bar // ".tools.btn_cockpit_visibility")) &
+        label = "Toggle Sidebar::Ctrl+J" &
+        commands = "if condition=(.gui.ppt_main.sash1.sash2.cockpit.displayed)", &
+                "    mdi toolbar display toolbar=dashboard state=off", &
+                "    int tog set state=off tog=.gui.ppt_main.ppt_tbar1.cockpit_visibility", &
+                    "else", &
+                    "    mdi toolbar display toolbar=dashboard state=on", &
+                    "    int tog set state=on tog=.gui.ppt_main.ppt_tbar1.cockpit_visibility", &
+                    "end" !"
+
+    ! Free Body Diagram Button
+    interface separator create separator_name=(eval(.mdi.menu_bar // ".tools.fbd_sep"))
+    interface push_button create   &
+        push_button_name=(eval(.mdi.menu_bar // ".tools.btn_fbd")) &
+        label = "Free Body Diagram..." &
+        commands = "interface dialog display dialog=.gui.dbox_fbd"
+
+    ! Force Graphics
+    interface push_button create   &
+        push_button_name=(eval(.mdi.menu_bar // ".tools.btn_all_force_graphics")) &
+        label = "Turn on all force graphics" &
+        commands = "athorn all_force_graphics"
+end
+
 
 ! ---------------------------------------------------------------------
 ! Run Python Setup
