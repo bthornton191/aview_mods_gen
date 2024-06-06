23a7f9901ebe68f4100c57965a956b85d75ce8c0
Ben Thornton <ben.thornton@hexagon.com>
2024-06-06 09:14:09 -0400

    Refactored setup.py and added `check_cache()`

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ import os
 import sys
 from pathlib import Path
 
+AVIEW_MOD_GEN = Path('C:/Program Files/MSC.Software/Adams/aview_mods_gen')
 CODE_DIR = Path(os.environ['CODE_DIR'])
-
-# Add local libraries to path
+LOCAL_LIBS = ['aviewpy',
+              'thornpy',
+              'adamspy',
+              'adams_shell_wear']
 
 def add_libs_to_path(libs, libs_dir: Path):
 
@@ -13,12 +16,22 @@ def add_libs_to_path(libs, libs_dir: Path):
         if lib_dir not in sys.path:
             sys.path.insert(0, lib_dir)
 
+def check_cache():
+    bin_file = Path(os.environ['TOPDIR']) / 'custom_binaries/aview.bin'
+    bin_file.parent.mkdir(exist_ok=True)
+    user_aview_bs = Path(os.environ.get('MSC_ADAMS_STARTIN_DIR', Path.home())) / 'aviewBS.cmd'
+
+    use_cache = (bin_file.exists() 
+                 and all(is_newer(file, bin_file) for file in AVIEW_MOD_GEN.iterdir() if file.is_file())
+                 and (user_aview_bs.exists() is False or is_newer(bin_file, user_aview_bs)))
+    
+    return use_cache, bin_file.as_posix()
+
+def is_newer(file_a: Path, file_b: Path):
+    return file_b.stat().st_mtime > file_a.stat().st_mtime
 
 if __name__ == '__main__':
-    add_libs_to_path(['aviewpy',
-                      'thornpy',
-                      'adamspy',
-                      'adams_shell_wear'],
+    add_libs_to_path(LOCAL_LIBS,
                      libs_dir=CODE_DIR)
     
     from aviewpy.utils.excepthook import aview_excepthook                                           # isort: skip
